4215c478064a3523775eefb37f4882775a0b411e
Karimof <islamkarimov8580@gmail.com>
2023-09-16 13:21:41 +0500

    first init

---
+++ src/test/java/ux/experience/students/StudentsApplicationTests.java
@@ -0,0 +1,13 @@
+package ux.experience.students;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
