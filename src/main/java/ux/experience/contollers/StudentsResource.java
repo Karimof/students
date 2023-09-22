b99ed29fbb68349880a0bcc67a5a7e3a705bfa14
Karimof <islamkarimov8580@gmail.com>
2023-09-22 15:00:24 +0500

    code some cleanup

--- src/main/java/ux/experience/contollers/StudentsResource.java
+++ src/main/java/ux/experience/contollers/StudentsResource.java
@@ -1,5 +1,8 @@
 package ux.experience.contollers;
 
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -7,6 +10,7 @@ import ux.experience.domain.Students;
 import ux.experience.services.ImageUploadService;
 import ux.experience.services.StudentsService;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
@@ -38,6 +42,15 @@ public class StudentsResource {
     public ResponseEntity<Void> uploadAvatar(@RequestPart(value = "file") MultipartFile file) {
         imageUploadService.save(file);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping(value = "/avatar/{studentId}", produces = MediaType.IMAGE_JPEG_VALUE)
+    public ResponseEntity<Resource> getAvatar(@PathVariable("studentId") Long studentId) throws IOException {
+        ByteArrayResource studentAvatar = studentsService.getStudentAvatar(studentId);
+        if (studentAvatar == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok().body(studentAvatar);
     }
 
     @GetMapping("/students")
--- src/main/java/ux/experience/services/StudentsService.java
+++ src/main/java/ux/experience/services/StudentsService.java
@@ -1,9 +1,14 @@
 package ux.experience.services;
 
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.stereotype.Service;
 import ux.experience.domain.Students;
 import ux.experience.repositories.StudentsRepository;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
@@ -17,24 +22,41 @@ public class StudentsService {
         this.studentsRepository = studentsRepository;
     }
 
-    public Students save(Students student){
+    public Students save(Students student) {
         student.setCreatedAt(LocalDateTime.now());
         return studentsRepository.save(student);
     }
 
-    public Students update(Students student){
+    public Students update(Students student) {
         return studentsRepository.save(student);
     }
 
-    public List<Students> findAll(){
+    public List<Students> findAll() {
         return studentsRepository.findAll();
     }
 
-    public Optional<Students> findOne(Long id){
+    public Optional<Students> findOne(Long id) {
         return studentsRepository.findById(id);
     }
 
-    public void delete(Long id){
+    public ByteArrayResource getStudentAvatar(Long studentId) {
+        Optional<Students> optStudent = findOne(studentId);
+        if (optStudent.isPresent()) {
+            Students student = optStudent.get();
+            Path absolutePath = Paths.get(
+                    "D:/Programming/Projects/Experience projects/students/src/main/resources/avatars/"
+                            + student.getAvatar());
+            ByteArrayResource inputStream = null;
+            try {
+                inputStream = new ByteArrayResource(Files.readAllBytes(absolutePath));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            return inputStream;
+        } else return null;
+    }
+
+    public void delete(Long id) {
         studentsRepository.deleteById(id);
     }
 }
